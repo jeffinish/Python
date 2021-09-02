bb11e844fa5141ddbb17810eeb2a9a740fe2047a
Jefferson Silva <jeffauh@gmail.com>
2021-09-02 01:29:17 -0300

    Mais uma aula do Curso Explorando o Seaborn

--- Alura/Data Visualization - Seaborn/Script-Tip.py
+++ Alura/Data Visualization - Seaborn/Script-Tip.py
@@ -1,7 +1,7 @@
 #Importando as bibliotecas
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 pd.set_option("display.max_rows",10) #Seta o máximo de linhas para ser exibida
 pd.set_option("display.max_columns",6) #Seta o máximmo de colunas para ser exibida
@@ -61,14 +61,42 @@ data.Hora_do_dia = data.Hora_do_dia.map(hora)
 
 ### --- Análises Estatísticas --- ###
 
-## -- Conferindo a qualidade dos dados
+## -- Conferindo a qualidade dos dados -- ##
 
-print("A base de dados contém {} registros \n".format(data.shape[0]))
-print("Registros não nulos")
-print(data.count())
+#print("A base de dados contém {} registros \n".format(data.shape[0]))
+#print("Registros não nulos")
+#print(data.count())
 
 ## -- Valor da Conta x Gorjeta -- ##
 
-#valor_gorgeta = sns.scatterplot(x="Valor_da_Conta",y="Gorjeta",data=data)
+valor_gorgeta = sns.scatterplot(x="Valor_da_Conta",y="Gorjeta",data=data)
+valor_gorgeta.figure.suptitle("Análises Estatísticas") #Define um suptitulo ao gráfico gerado (Titulo acima do Titulo)
+valor_gorgeta.set_title("Valor da Conta x Gorjeta") # Define o titulo (Que na verdade fica abaixo do subtitulo)
+valor_gorgeta.set(xlabel = "Valor da Conta",ylabel="Valor da Gorgeta")
+valor_gorgeta.figure.savefig("Valor da Conta x Gorjeta.png")
 #plt.show()
 # - A primeira impressão que temos é que existe uma relação entre o valor da conta e o valor da gorjeta. Quanto maior o valor da conta, maior o valor de gorjeta dado.
+
+# - Criar o campo porcentagem - #
+
+print(data.head(1)) #Print para conferir
+data["Porcentagem"] = data["Gorjeta"]/data["Valor_da_Conta"] #Define uma nova coluna a dividindo a coluna Gorjeta pela coluna Valor_da_Conta.
+#print(data.head()) #Print para conferir
+data.Porcentagem = data.Porcentagem.round(2) #Arredonda a coluna Porcentagem com duas casas decimais
+#print(data.head(3))
+
+# - Analisando Valor da Conta x Porcentagem de Gorjeta
+porcentagem_conta = sns.scatterplot(x="Valor_da_Conta",y="Porcentagem",data=data)
+porcentagem_conta.figure.suptitle("Análises Estatísticas")
+porcentagem_conta.set_title("Valor da Conta x Porcentagem de Gorjeta")
+porcentagem_conta.set(xlabel = "Valor da Conta", ylabel="Porcentagem de Gorjeta")
+porcentagem_conta.figure.savefig("Valor da Conta x Porcentagem de Gorjeta.png")
+# - Apesar de termos que o valor da gorjeta aumenta juntamente com o valor da conta, percebemos neste último gráfico que este aumento não é proporcional, isto é, o aumento da gorjeta não é proporcional ao aumento da conta.
+
+# - Criando gráficos de maneira diferente - #
+#sns.relplot(x="Valor_da_Conta",y="Porcentagem",kind="line",data=data) #Esse gráfico é meio bosta com o argumento kind=Line.
+reg_porcentagem_conta = sns.lmplot(x="Valor_da_Conta", y="Porcentagem", data=data)
+reg_porcentagem_conta.figure.suptitle("Análies Estatísticas")
+#reg_porcentagem_conta.set_title("Valor da Conta x Gorjeta - Regressão Linear")
+reg_porcentagem_conta.set(xlabel = "Valor da Conta",ylabel="Valor da Gorgeta")
+reg_porcentagem_conta.figure.savefig("Regressão Linear Valor da Conta x Porcentagem.png")
